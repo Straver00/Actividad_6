b43dc3ee1e9ab4617ff24d657d3817d336c13b6f
Straver <macuino.555@gmail.com>
2023-06-28 00:47:15 -0500

    Update
    Crud.

---
+++ Crud1/src/main/java/com/mycompany/crud1/Crud1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud1;
+
+/**
+ *
+ * @author macui
+ */
+public class Crud1 {
+
+    public static void main(String[] args) {
+        Ventana ventana= new Ventana();
+        ventana.setVisible(true);
+    }
+}
